ce31fd3ab40e3c72a5534a92efd5fbe3c5e4e1ac
tomoszkfw <160325821+tomoszkfw@users.noreply.github.com>
2024-07-02 23:12:06 +0900

    feat: リアクション機能を実装

--- main.mjs
+++ main.mjs
@@ -11,9 +11,13 @@ const prisma = new PrismaClient();
 const template = fs.readFileSync("./template.html", "utf-8");
 app.get("/", async (request, response) => {
     const posts = await prisma.post.findMany();
-    const html = template.replace(
+    const displayMessages = template.replace(
         "<!-- posts -->",
         posts.map((post) => `<li>${escapeHTML(post.message)}</li>`).join("")
+    );
+    const html = displayMessages.replace(
+        "<!-- options -->",
+        posts.map((post) => `<option value=${post.id}>id:${post.id}</option>`)
     );
     response.send(html);
 });
@@ -23,6 +27,22 @@ app.post("/send", async (request, response) => {
         data: { message: request.body.message },
     });
     response.redirect("/");
+});
+
+app.post("/reaction", async (req, res) => {
+    const reactionId = Number(await req.body.reactionId);
+    let post = await prisma.post.findUnique({
+        where: { id: reactionId },
+    });
+    if (req.body.reaction === "good") {
+        post.good += 1;
+    } else if (req.body.reaction === "favorite") {
+        post.favorite += 1;
+    } else {
+        post.bad += 1;
+    }
+    await prisma.post.update({ where: { id: reactionId }, data: { ...post } });
+    res.redirect("/");
 });
 
 app.listen(3000);
--- static/script.js
+++ static/script.js
@@ -1,7 +1,15 @@
 const messageInput = document.getElementById("message-input");
+const reactionId = document.getElementById("reactionId-input");
+const good = document.getElementById("good");
+const favorite = document.getElementById("favorite");
+const bad = document.getElementById("bad");
+
+const noMessage = messageInput.value === "";
+const noId = reactionId.value === "";
+const noSelection = !good.checked && !favorite.checked && !bad.checked;
 
 setInterval(() => {
-  if (messageInput.value === "") {
-    window.location.reload();
-  }
+    if (noMessage && noId && noSelection) {
+        window.location.reload();
+    }
 }, 5 * 1000);
