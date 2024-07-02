9bc5b9dfc437211c3793d1a83aac18a7e39fbadf
tomoszkfw <160325821+tomoszkfw@users.noreply.github.com>
2024-07-02 18:27:44 +0900

    format

--- main.mjs
+++ main.mjs
@@ -1,7 +1,7 @@
-import fs from "node:fs";
-import express from "express";
 import { PrismaClient } from "@prisma/client";
 import escapeHTML from "escape-html";
+import express from "express";
+import fs from "node:fs";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -10,19 +10,19 @@ const prisma = new PrismaClient();
 
 const template = fs.readFileSync("./template.html", "utf-8");
 app.get("/", async (request, response) => {
-  const posts = await prisma.post.findMany();
-  const html = template.replace(
-    "<!-- posts -->",
-    posts.map((post) => `<li>${escapeHTML(post.message)}</li>`).join(""),
-  );
-  response.send(html);
+    const posts = await prisma.post.findMany();
+    const html = template.replace(
+        "<!-- posts -->",
+        posts.map((post) => `<li>${escapeHTML(post.message)}</li>`).join("")
+    );
+    response.send(html);
 });
 
 app.post("/send", async (request, response) => {
-  await prisma.post.create({
-    data: { message: request.body.message },
-  });
-  response.redirect("/");
+    await prisma.post.create({
+        data: { message: request.body.message },
+    });
+    response.redirect("/");
 });
 
 app.listen(3000);
