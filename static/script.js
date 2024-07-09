172be24d86060cfed4a498132ba3683a5ea3049b
tomoszkfw <160325821+tomoszkfw@users.noreply.github.com>
2024-07-09 17:54:23 +0900

    feat: メッセージがない時は投稿できないようにする

--- static/script.js
+++ static/script.js
@@ -2,6 +2,17 @@ const messageInput = document.getElementById("message-input");
 const reactionIdInput = document.getElementById("reactionIdSelect");
 const selectReaction = document.getElementById("reaction");
 
+// メッセージがない時は送信ボタンを disabled にする
+const submitPostBtn = document.getElementById("submitPost");
+messageInput.onchange = () => {
+    if (!messageInput.value) {
+        submitPostBtn.disabled = true;
+    } else {
+        submitPostBtn.disabled = false;
+    }
+};
+
+// 入力内容がないときは 5 秒おきにリロード
 setInterval(() => {
     if (
         !messageInput.value &&
