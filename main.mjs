65fc974e354b8a91e7c8baa74f77272c5c3c430f
tomoszkfw <160325821+tomoszkfw@users.noreply.github.com>
2024-07-09 18:13:08 +0900

    fix: searchTemplate を定義

--- main.mjs
+++ main.mjs
@@ -1,7 +1,7 @@
-import fs from "node:fs";
-import express from "express";
 import { PrismaClient } from "@prisma/client";
 import escapeHTML from "escape-html";
+import express from "express";
+import fs from "node:fs";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -9,37 +9,40 @@ app.use(express.static("static"));
 const prisma = new PrismaClient();
 
 const template = fs.readFileSync("./template.html", "utf-8");
+const searchTemplate = fs.readFileSync("./searchTemplate.html", "utf-8");
 app.get("/", async (request, response) => {
-  const posts = await prisma.post.findMany();
-  const html = template.replace(
-    "<!-- posts -->",
-    posts.map((post) => `<li>${escapeHTML(post.message)}</li>`).join(""),
-  );
-  response.send(html);
+    const posts = await prisma.post.findMany();
+    const html = template.replace(
+        "<!-- posts -->",
+        posts.map((post) => `<li>${escapeHTML(post.message)}</li>`).join("")
+    );
+    response.send(html);
 });
 
 app.post("/send", async (request, response) => {
-  await prisma.post.create({
-    data: { message: request.body.message },
-  });
-  response.redirect("/");
+    await prisma.post.create({
+        data: { message: request.body.message },
+    });
+    response.redirect("/");
 });
 
 app.post("/search", async (request, response) => {
-  const searchPosts = await prisma.post.findMany({
-    where: {
-      message: {
-        contains: request.body.keyword,  
-      },
-    },
-  });
+    const searchPosts = await prisma.post.findMany({
+        where: {
+            message: {
+                contains: request.body.keyword,
+            },
+        },
+    });
 
-  const searchHtml = searchTemplate.replace(
-    "<!-- posts -->",
-    searchPosts.map((post) => `<li>${escapeHTML(post.message)}</li>`).join(""),
-  );
-  
-  response.send(searchHtml)
+    const searchHtml = searchTemplate.replace(
+        "<!-- posts -->",
+        searchPosts
+            .map((post) => `<li>${escapeHTML(post.message)}</li>`)
+            .join("")
+    );
+
+    response.send(searchHtml);
 });
 
 app.listen(3000);
